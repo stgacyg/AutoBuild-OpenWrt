e422b7feebfe9584f7d55404eb3da7da97100cb1
Cnbbx <admin@cnbbx.com>
2024-06-03 23:13:26 +0800

    Update d-team_newifi-d2.sh

--- d-team_newifi-d2.sh
+++ d-team_newifi-d2.sh
@@ -8,7 +8,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.0./192.168.99./g' feeds/luciApp/applications/luci-app-pptp-server/root/etc/config/pptpd
 
 # Modify hostname
 sed -i 's/OpenWrt/Cnbbx/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
