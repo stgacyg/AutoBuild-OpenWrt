783cf0b368384d33064315da368633bfe5837c9e
Cnbbx <admin@cnbbx.com>
2024-06-03 21:27:33 +0800

    Update d-team_newifi-d2.sh

--- d-team_newifi-d2.sh
+++ d-team_newifi-d2.sh
@@ -22,24 +22,6 @@ sed -i '/set wireless.radio${devidx}.disabled/d' package/kernel/mac80211/files/l
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
-# Modify Packages
-sed -i 's/+ariang//g'  feeds/luci/applications/luci-app-aria2/Makefile
-sed -i 's/+alist//g'  feeds/luciApp/applications/luci-app-alist/Makefile
-
-# Compressed Go
-sed -i '/upx --lzma --best/d' .config
-sed -i -e "/GoPackage/a  \\\t$\(STAGING_DIR_HOST\)\/bin\/upx --lzma --best $\(PKG_INSTALL_DIR\)\/usr\/bin\/main \| true" feeds/small/xray-core/Makefile
-sed -i '/upx --lzma --best/d' feeds/packages/net/frp/Makefile
-sed -i -e "/define Package\/frp\/install/a  \\\t$\(STAGING_DIR_HOST\)\/bin\/upx --lzma --best $\(PKG_INSTALL_DIR\)\/usr\/bin\/$\(2\) \| true" feeds/packages/net/frp/Makefile
-
-# ntfs-3g
-sed -i '/upx --lzma --best/d' feeds/packages/utils/ntfs-3g/Makefile
-sed -i -e "/\/mount.ntfs-3g/a  \\\t$\(LN\) ..\/usr\/bin\/ntfs-3g $\(1\)\/sbin\/mount.ntfs" feeds/packages/utils/ntfs-3g/Makefile
-
-# remove
-cp -f /dev/null feeds/small/luci-app-ssr-plus/root/etc/ssrplus/gfw_list.conf
-cp -f /dev/null feeds/small/luci-app-ssr-plus/root/etc/ssrplus/china_ssr.txt
-
 # Rom Size
 sed -i '/Rom Size/d' .config
 sed -i '/CONFIG_TARGET_KERNEL_PARTSIZE/d' .config
@@ -55,6 +37,9 @@ sed -i '/CONFIG_VERSION_DIST/d' .config
 sed -i '/CONFIG_VERSION_NUMBER/d' .config
 sed -i '/CONFIG_VERSION_CODE/d' .config
 sed -i '/CONFIG_VERSION_HOME_URL/d' .config
+sed -i '/CONFIG_VERSION_REPO/d' .config
+sed -i '/CONFIG_VERSION_PRODUCT/d' .config
+sed -i '/CONFIG_VERSION_HWREV/d' .config
 sed -i '/Image Configurations/d' .config
 echo '# Image Configurations' >> .config
 echo 'CONFIG_IMAGEOPT=y' >> .config
@@ -62,9 +47,10 @@ echo 'CONFIG_VERSIONOPT=y' >> .config
 echo 'CONFIG_VERSION_DIST="Cnbbx"' >> .config
 echo 'CONFIG_VERSION_NUMBER="R22.03"' >> .config
 echo "CONFIG_VERSION_CODE=\"build $(TZ=UTC-8 date "+%Y.%m.%d")"\" >> .config
-echo 'CONFIG_VERSION_HOME_URL="http://autobuild.i.cnbbx.com/"' >> .config
-
-# sed -i 's/not//g'  feeds/luci/modules/luci-base/src/mkversion.sh
+echo 'CONFIG_VERSION_HOME_URL="https://autobuild.i.cnbbx.com/"' >> .config
+echo 'CONFIG_VERSION_REPO="https://autobuild.i.cnbbx.com/"' >> .config
+echo 'CONFIG_VERSION_PRODUCT="CnbbxOS"' >> .config
+echo 'CONFIG_VERSION_HWREV="$(TZ=UTC-8 date "+%Y.%m.%d")"' >> .config
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
@@ -76,9 +62,4 @@ echo 'CONFIG_VERSION_HOME_URL="http://autobuild.i.cnbbx.com/"' >> .config
     echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
     sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1"GitHub Actions"@' .config
 
-# Replace xray-core
-cd feeds/packages/net/
-rm -rf xray-core
-ln -sf ../../../feeds/small/xray-core/ xray-core
-cd ../../../
 ./scripts/feeds install -a
