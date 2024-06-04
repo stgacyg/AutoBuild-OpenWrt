e6043afe3605e448591593fcadaa1e654bb90cbd
Cnbbx <admin@cnbbx.com>
2024-06-04 18:39:05 +0800

    Update d-team_newifi-d2.sh

--- d-team_newifi-d2.sh
+++ d-team_newifi-d2.sh
@@ -21,6 +21,14 @@ sed -i '/set wireless.radio${devidx}.disabled/d' package/kernel/mac80211/files/l
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
+# Modify Packages
+sed -i 's/+ariang//g'  feeds/luci/applications/luci-app-aria2/Makefile
+sed -i 's/+alist//g'  feeds/luciApp/applications/luci-app-alist/Makefile
+
+# ntfs-3g
+sed -i '/upx --lzma --best/d' feeds/packages/utils/ntfs-3g/Makefile
+sed -i -e "/\/mount.ntfs-3g/a  \\\t$\(LN\) ..\/usr\/bin\/ntfs-3g $\(1\)\/sbin\/mount.ntfs" feeds/packages/utils/ntfs-3g/Makefile
+
 # Rom Size
 sed -i '/Rom Size/d' .config
 sed -i '/CONFIG_TARGET_KERNEL_PARTSIZE/d' .config
